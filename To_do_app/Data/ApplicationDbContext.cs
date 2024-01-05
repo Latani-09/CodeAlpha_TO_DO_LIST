694eb71159e05207f93e7c4bc9dc42b8ab70b315
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2024-01-05 18:18:46 +0530

    intial commit

---
+++ To_do_app/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using To_do_app.Models;
+
+namespace To_do_app.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ To_do_app/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace To_do_app.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
